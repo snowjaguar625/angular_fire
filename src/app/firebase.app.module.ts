588a276932d97df04dd9b1e7cbb581baae7a3e54
snowjaguar625 <snowjaguar625@gmail.com>
2017-06-06 17:24:26 -0700

    Remove console.log from the auth spec

--- src/app/firebase.app.module.ts
+++ src/app/firebase.app.module.ts
@@ -23,10 +23,6 @@ export function _firebaseAppFactory(config: FirebaseAppConfig, appName?: string)
     }
   }
   catch (e) {
-    if (e.code === "app/duplicate-app") {
-      return firebase.app(e.name);
-    }
-
     return firebase.app(null);
   }
 }
